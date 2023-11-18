293fee0a122a27e7b0fc58e9c8b9ee574f464b4c
german pavez <galbpavezm@falabella.cl>
2023-11-17 23:20:26 -0300

    ff

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 	r := gin.Default()
 	corsConfig := cors.DefaultConfig()
 
-	corsConfig.AllowOrigins = []string{"https://web-access-control-production.up.railway.app/", "http://127.0.0.1:5500"}
+	corsConfig.AllowOrigins = []string{"https://web-access-control-production.up.railway.app"}
 	corsConfig.AllowCredentials = true
 	corsConfig.AddAllowMethods("OPTIONS")
 	r.Use(cors.New(corsConfig))
